178f89581f71cd71d60b9fb2bdc61be9bdaa9fd7
singlanehal <136306866+singlanehal@users.noreply.github.com>
2024-04-07 23:43:01 +0530

    Create three.c

---
+++ three.c
@@ -0,0 +1,16 @@
+# power-of-three
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0) return false;
+        while(1){
+            if(n==1 || n==3)
+            {
+                return true;
+            }
+            if(n%3!=0) return false;
+            n=n/3;
+        }
+        return true;
+    }
+};
